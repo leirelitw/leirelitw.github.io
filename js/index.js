7166d3ddc263f7e31d93fdfaa42b192a6ccf12f8
Leire Litwin <leirelitwin@MacBook-Pro-de-Leire.local>
2019-03-17 14:41:49 +0100

    update seo
    update seo

--- js/index.js
+++ js/index.js
@@ -88,10 +88,17 @@ var modal = document.getElementById('myModal');
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+//var closemodal = document.getElementsByClassName("modal")[0];
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
+//closemodal.onclick = function() {
+//  modal.style.display = "none";
+//}
 
 function openModal(imageToOpen){
   // Get the modal
